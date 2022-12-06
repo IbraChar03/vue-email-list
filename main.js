ceceb8ed7752eb45e0fcbf2f8cab5cb6610a8d38
Ibrahim <ibrachar68@gmail.com>
2022-12-06 16:29:17 +0100

    creazione 10 email

--- main.js
+++ main.js
@@ -9,13 +9,6 @@ const {createApp} = Vue;
     
     },
     methods:{
-          apiEmail(){
-            axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-            .then((result) => {
-              console.log(result.data.response);             
-            });
-          
-          }
                   
    },
    mounted(){   
