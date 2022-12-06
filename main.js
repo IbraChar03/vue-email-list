9ca4ac58a65131c696a2c4ed422e6ddd3adff550
Ibrahim <ibrachar68@gmail.com>
2022-12-06 15:43:15 +0100

    email v-for

--- main.js
+++ main.js
@@ -4,30 +4,31 @@ const {createApp} = Vue;
 
     data(){
         return{
-           
+           email:``,
         }
     
     },
     methods:{
-         apiEmail(){
-           axios.get(`https:flynn.boolean.careers/exercises/api/random/mail`)
-           .then((result) => {
-             console.log(result.data.response);
-             let email=result.data.response;
-             let ul=document.getElementById("list")
-             let element=document.createElement("li")
-             element.innerHTML= email
-             ul.append(element)
-           });
-         }
+          apiEmail(){
+            axios.get(`https:flynn.boolean.careers/exercises/api/random/mail`)
+            .then((result) => {
+              console.log(result.data.response);
+              this.email=result.data.response;
+              //   let ul=document.getElementById("list")
+              //  let element=document.createElement("li")
+              // element.innerHTML= this.email
+              //  ul.append(element)
+            });
+          
+          
+          }
                   
    },
    mounted(){   
-      for (let index = 0; index < 10; index++) {
-          this.apiEmail()
-      }
- 
+     this.apiEmail()
+     
   }
+
 
 }).mount("#app")
 
